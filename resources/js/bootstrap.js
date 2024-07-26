67def8434056ae4a4f7565f91795f73c9d0cf641
Giant Jb <arjunadeland400@gmail.com>
2024-07-26 10:17:02 +0700

    update:setup echo with socket

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,18 +7,22 @@ import DataTable from "datatables.net-dt";
 import Echo from "laravel-echo";
 import socketio from "socket.io-client";
 import Swal from "sweetalert2";
+import 'datatables.net-dt/js/dataTables.tailwindcss.js';
 const csrfToken = document.head.querySelector(
     'meta[name="csrf-token"]'
 ).content;
 window.axios.defaults.headers.common["X-CSRF-TOKEN"] = csrfToken;
 window.axios.defaults.headers.common["Accept"] = "application/json";
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-import 'datatables.net-dt/js/dataTables.tailwindcss.js';
-
 window.DataTable = DataTable;
 window.Swal = Swal;
 window.axios = axios;
 window.$ = $;
 window.jQuery = jQuery;
 window.axios = axios;
+window.Echo = new Echo({
+    client: socketio,
+    broadcaster: "socket.io",
+    host: window.location.hostname + ":6001",
+});
 select2();
