f0cfc8db34c3bc05e7006e2532f45d2d89d1313f
Giant Jb <arjunadeland400@gmail.com>
2024-07-26 09:51:08 +0700

    add:jquery

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 import "flowbite";
-window.axios = axios;
+import $ from "jquery";
+import jQuery from "jquery";
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+const csrfToken = document.head.querySelector(
+    'meta[name="csrf-token"]'
+).content;
+window.axios.defaults.headers.common["X-CSRF-TOKEN"] = csrfToken;
+window.axios.defaults.headers.common["Accept"] = "application/json";
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+window.axios = axios;
+window.$ = $;
+window.jQuery = jQuery;
+window.axios = axios;
