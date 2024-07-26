9fd733efe42421fbddbbc6ffb6a61841c1e4d589
Giant Jb <arjunadeland400@gmail.com>
2024-07-26 10:05:47 +0700

    update

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,6 +6,7 @@ import select2 from "select2";
 import DataTable from "datatables.net-dt";
 import Echo from "laravel-echo";
 import socketio from "socket.io-client";
+import Swal from "sweetalert2";
 const csrfToken = document.head.querySelector(
     'meta[name="csrf-token"]'
 ).content;
@@ -13,6 +14,8 @@ window.axios.defaults.headers.common["X-CSRF-TOKEN"] = csrfToken;
 window.axios.defaults.headers.common["Accept"] = "application/json";
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
+window.DataTable = DataTable;
+window.Swal = Swal;
 window.axios = axios;
 window.$ = $;
 window.jQuery = jQuery;
