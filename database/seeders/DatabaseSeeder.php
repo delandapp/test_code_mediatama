e8d318b5f6aee99cb49f271dddbcbb86cab6e81e
delandapp <alihandronikolas@gmail.com>
2024-07-26 22:58:15 +0700

    add:seederUser

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(UserCreateSeeder::class);
     }
 }
---
+++ database/seeders/UserCreateSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserCreateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $password = "Password124@";
+        $superadmin = User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
+            'password' => bcrypt($password),
+        ]);
+
+        $marketing = User::create([
+            'name' => 'Marketing',
+            'email' => 'marketing@gmail.com',
+            'password' => bcrypt($password),
+        ]);
+
+        $finance = User::create([
+            'name' => 'Finance',
+            'email' => 'finance@gmail.com',
+            'password' => bcrypt($password),
+        ]);
+    }
+}
