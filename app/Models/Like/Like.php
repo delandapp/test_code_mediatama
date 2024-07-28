7caac474e41510559e22a3b4d9cd596147b75e15
delandapp <alihandronikolas@gmail.com>
2024-07-29 01:00:04 +0700

    update:LikeModel

--- app/Models/Like/Like.php
+++ app/Models/Like/Like.php
@@ -2,10 +2,26 @@
 
 namespace App\Models\Like;
 
+use App\Models\Materi\Materi;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Like extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $table = 'tabel_like';
+    protected $guarded = ['id'];
+    public $timestamps = true;
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function materis()
+    {
+        return $this->belongsTo(Materi::class, 'materi_id', 'id');
+    }
 }
