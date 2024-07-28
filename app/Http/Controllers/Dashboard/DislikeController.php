ce00ec0915aec856b9de70122cf78381353d7bcb
delandapp <alihandronikolas@gmail.com>
2024-07-29 00:58:14 +0700

    update:DislikeController

--- app/Http/Controllers/Dashboard/DislikeController.php
+++ app/Http/Controllers/Dashboard/DislikeController.php
@@ -7,5 +7,23 @@ use Illuminate\Http\Request;
 
 class DislikeController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+
+        try {
+            $request->validate([
+                'video_id' => 'required|exists:video_materials,id',
+                'user_id' => 'required|exists:users,id',
+            ]);
+
+            $dislike = auth()->user()->dislikes()->create([
+                'video_id' => $request->video_id,
+                'user_id' => $request->user_id,
+                'status' => true,
+            ]);
+            return response()->json(['status' => true, 'data' => $dislike, 'message' => 'Dislike created successfully'], 200);
+        } catch (\Throwable $th) {
+            return response()->json(['message' => false, 'errors' => $th, 'message' => $th->getMessage()], 500);
+        }
+    }
 }
